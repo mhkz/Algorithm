8a2b531e10423f61951a4fb616b4da763167e83c
feng_guo <one778@163.com>
2021-12-18 18:46:28 +0800

    lc_02_两数相加

--- Leetcode/src/Code_02/Solution.java
+++ Leetcode/src/Code_02/Solution.java
@@ -7,27 +7,22 @@ package Code_02;
  */
 public class Solution {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		// 创建一个头节点
-        ListNode root = new ListNode(0);
-        // 拿到节点索引
-        ListNode cursor = root;
-        
-        // 记录进位
-        int carry = 0;
-        while(l1 != null || l2 != null || carry != 0) {
-            int l1Val = l1 != null ? l1.val : 0;
-            int l2Val = l2 != null ? l2.val : 0;
-            int sumVal = l1Val + l2Val + carry;
-            carry = sumVal / 10;
-            
-            ListNode sumNode = new ListNode(sumVal % 10);
-            cursor.next = sumNode;
-            cursor = sumNode;
-            
-            if(l1 != null) l1 = l1.next;
-            if(l2 != null) l2 = l2.next;
-        }
-        
-        return root.next;
+		// 创建虚拟头节点
+		ListNode dummyHead = new ListNode(0);
+		ListNode cursor = dummyHead;
+		int carry = 0;
+		while(l1 != null || l2 != null || carry != 0) {
+			int v1 = l1 != null ? l1.val : 0;
+			int v2 = l2 != null ? l2.val : 0;
+			int sum = v1 + v2 + carry;
+			carry = sum / 10;
+			ListNode node = new ListNode(sum % 10);
+			cursor.next = node;
+			cursor = node;
+			
+			if(l1 != null) l1 = l1.next;
+			if(l2 != null) l2 = l2.next;
+		}
+		return dummyHead.next;
     }
 }
